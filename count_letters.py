c0d24e6184d973d030c3be66452b466cb3f9ea60
Ken Hileman <ken.hileman@outlook.com>
2024-05-21 21:05:02 -0400

    8a

---
+++ count_letters.py
@@ -0,0 +1,20 @@
+# Author: Kenneth Hileman
+# GitHub username: PHTIWringer
+# NOTE: Will not have a ReadMe - Using another IDE
+# Date: 05/15/2024
+# Description: Program that a string as a parameter and returns a dictionary that tabulates how many of each letter is in that string.
+
+def count_letters(input_string):
+    '''Function that takes in a string and returns each letter and the a count of how many are shared'''
+    letter_counts = {}
+
+    for char in input_string:
+        if 'A' <= char <= 'Z' or 'a' <= char <= 'z':
+            upper_char = char.upper()
+            if upper_char in letter_counts:
+                letter_counts[upper_char] += 1
+            else:
+                letter_counts[upper_char] = 1
+    return letter_counts
+
+print(count_letters("AaBb"))
